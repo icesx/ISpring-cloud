db0029dcf82c87b7191ac64d186c419d00af3f31
chengtao <chengtao@ithink>
2020-06-27 08:44:51 +0800

    change all application.yml to k8s

--- spring-cloud-sample/spring-cloud-dashboard/src/main/java/cn/xportal/sc/dashboard/DashboardApplication.java
+++ spring-cloud-sample/spring-cloud-dashboard/src/main/java/cn/xportal/sc/dashboard/DashboardApplication.java
@@ -2,6 +2,7 @@ package cn.xportal.sc.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @SpringBootApplication
 @EnableTurbine
 @EnableHystrixDashboard 
+@ConfigurationProperties("classpath:application.yml")
 public class DashboardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardApplication.class, args);
--- spring-cloud-sample/spring-cloud-eureka/src/main/java/cn/xportal/sc/eureka/EurekaApplication.java
+++ spring-cloud-sample/spring-cloud-eureka/src/main/java/cn/xportal/sc/eureka/EurekaApplication.java
@@ -2,6 +2,7 @@ package cn.xportal.sc.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
