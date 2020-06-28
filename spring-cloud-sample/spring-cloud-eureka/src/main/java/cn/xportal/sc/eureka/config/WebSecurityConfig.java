6456c3a2e96286d88c8ff222684effee8cfd3947
chengtao <chengtao@ithink>
2020-06-28 11:19:21 +0800

    runed on k8s

--- spring-cloud-sample/spring-cloud-eureka/src/main/java/cn/xportal/sc/eureka/config/WebSecurityConfig.java
+++ spring-cloud-sample/spring-cloud-eureka/src/main/java/cn/xportal/sc/eureka/config/WebSecurityConfig.java
@@ -1,15 +1,19 @@
 package cn.xportal.sc.eureka.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/eureka/**");
-        super.configure(http);
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().ignoringAntMatchers("/eureka/**");
+		logger.info("configure..");
+		super.configure(http);
+	}
 }
